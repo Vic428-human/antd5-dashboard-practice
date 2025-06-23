f49838c71265e740a99c09dc643d03ddc7ad42c3
Vic428-human <z0983195379a@gmail.com>
2025-06-23 21:42:46 +0800

    用v4版本的tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./queryClient.ts";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
