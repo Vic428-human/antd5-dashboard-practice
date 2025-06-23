2719ef8a4c0c0ab768b9d9a56b27851cc3db960c
Vic428-human <z0983195379a@gmail.com>
2025-06-23 21:07:45 +0800

    first commit for antd5

---
+++ README.md
@@ -0,0 +1,26 @@
+## 專案當前進度
+
+> 已經有基本的 tanstack query hook 的用法了，等哪天需要用到 redux toolkit ，需要複習的時候，我再把 redux toolkit 引用到該專案，
+> 專案更新至 2025/0614
+
+### 專案啟用 React + TypeScript + Vite + redux toolkit
+
+```
+npm i
+nvm use 18
+npm run dev
+
+```
+
+- [useHook 基礎的 tanstack-query 基本用法](https://github.com/Vic428-human/redux-toolkit-and-tanstack-query-demo/blob/main/src/hooks/useApplimittation.ts)
+- [QueryClient 獨立出來](https://github.com/Vic428-human/redux-toolkit-and-tanstack-query-demo/blob/main/src/hooks/useApplimittation.ts)
+
+### 專案用途
+
+> 結合 Redux Toolkit + TanStack Query (React Query) + vite 的 demo
+
+#### 一開始建置 vite + react + typescript 專案
+
+```js
+npm create vite@latest my-app -- --template react-ts
+```
---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+// import { useApplication } from "./hooks/useApplication.tsx";
+// import { queryClient } from "./queryClient.ts";
+
+function App() {
+  // const { data, isLoading, error } = useApplication();
+
+  // const renderContent = () => {
+  //   if (data) {
+  //     return (
+  //       <>
+  //         <h2>直接調用 tanstack的 custom hook</h2>
+  //         <pre>{JSON.stringify(data, null, 2)}</pre>
+  //       </>
+  //     );
+  //   }
+  //   if (isLoading) return <p>Loading...</p>;
+  //   if (error) return <p>Error: {error.message}</p>;
+  //   return <p>No data available.</p>;
+  // };
+
+  // const test = queryClient.getQueriesData({ queryKey: ["application"] });
+  // const applicationQueryData = test.length > 0 ? test[0][1] : null;
+
+  return (
+    <div>
+      <h1>Demo</h1>
+      {/* {renderContent()}
+      <h3>queryClient.getQueriesData 方式直接調用</h3>
+      {applicationQueryData ? (
+        <pre>{JSON.stringify(applicationQueryData, null, 2)}</pre>
+      ) : (
+        <p>No cached data found in queryClient.</p>
+      )} */}
+    </div>
+  );
+}
+
+export default App;
