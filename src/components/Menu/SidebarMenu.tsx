b6ccc7c0cd56d3df77304b9ae1d14c2ad751aa77
Vic428-human <z0983195379a@gmail.com>
2025-06-24 22:02:52 +0800

    sidebarmenu 組件獨立出來

---
+++ src/components/Menu/SidebarMenu.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Menu } from "antd";
+import type { MenuProps } from "antd";
+import {
+  UserOutlined,
+  DesktopOutlined,
+  TeamOutlined,
+  FileOutlined,
+} from "@ant-design/icons";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem("首頁", "/", <DesktopOutlined />),
+  getItem("測試菜單", "/menu-1", <UserOutlined />, [
+    getItem("Tom", "/menu-1/tom"),
+    getItem("Bill", "/menu-1/bill"),
+    getItem("Alex", "/menu-1/alex"),
+  ]),
+  getItem("Team", "/team", <TeamOutlined />, [
+    getItem("Team 1", "6"),
+    getItem("Team 2", "8"),
+  ]),
+  getItem("Files", "/file", <FileOutlined />),
+];
+
+interface SidebarMenuProps {
+  onMenuClick: MenuProps["onClick"];
+}
+
+const SidebarMenu: React.FC<SidebarMenuProps> = ({ onMenuClick }) => (
+  <Menu
+    theme="dark"
+    defaultSelectedKeys={["1"]}
+    mode="inline"
+    items={items}
+    onClick={onMenuClick}
+  />
+);
+
+export default SidebarMenu;
