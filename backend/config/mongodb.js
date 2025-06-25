45d40a63ae62b2d9078c92b54536dedf5907a52f
Vic428-human <z0983195379a@gmail.com>
2025-06-26 01:47:21 +0800

    fix: mondb url 錯誤

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     mongoose.connection.on('connected', () => console.log("mongodb 數據庫連線中"));
-    await mongoose.connect(`${process.env.MONGO_URI}/mern-auth`);
+    await mongoose.connect(`${process.env.MONGO_URI}-auth`);
 };
 
 export default connectDB;
