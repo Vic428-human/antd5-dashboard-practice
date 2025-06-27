74af938fa62123980c62c2cd55815ae6c92eb3b0
Vic428-human <z0983195379a@gmail.com>
2025-06-28 04:20:32 +0800

    feat: 添加otp驗證碼發送路由

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
-import { register, login, logout } from "../controller/authController.js";
+import { register, login, logout, sendOtpVerification } from "../controller/authController.js";
 import express from "express";
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
+authRouter.post("/sendOtpVerification", sendOtpVerification);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 
