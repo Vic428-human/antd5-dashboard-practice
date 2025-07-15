5d6a6589074fddf5f0faecc525ba65f041902d36
Vic428-human <z0983195379a@gmail.com>
2025-07-15 19:56:10 +0800

    feat:rwd

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,22 +3,141 @@ import { assets } from "../assets/assets.ts";
 import LuckyExchangeButton from "../components/LuckyExchangeButton.tsx";
 const Home = () => {
   return (
-    <div className="max-h-screen bg-[#302200]">
-      <div className="w-72 rounded-xl overflow-hidden shadow p-4">
-        <div
-          className="text-center bg-[#8A4F0E] text-white text-lg font-semibold mb-3"
-          style={{ borderTopLeftRadius: "16px", borderTopRightRadius: "16px" }}
-        >
-          Template
+    <div className="flex max-h-screen bg-[#302200]">
+      <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 gap-4">
+        <div className="w-72 rounded-xl overflow-hidden shadow p-4">
+          <div
+            className="text-center bg-[#8A4F0E] text-white text-lg font-semibold mb-3"
+            style={{
+              borderTopLeftRadius: "16px",
+              borderTopRightRadius: "16px",
+            }}
+          >
+            Template
+          </div>
+          {/* // 顯示 ON 狀態 */}
+          <LuckyExchangeButton isActive={true}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+          {/* // 顯示 OFF 狀態 */}
+          <LuckyExchangeButton isActive={false}>
+            Lucky Exchange
+          </LuckyExchangeButton>
         </div>
-        {/* // 顯示 ON 狀態 */}
-        <LuckyExchangeButton isActive={true}>
-          Lucky Exchange
-        </LuckyExchangeButton>
-        {/* // 顯示 OFF 狀態 */}
-        <LuckyExchangeButton isActive={false}>
-          Lucky Exchange
-        </LuckyExchangeButton>
+        <div className="w-72 rounded-xl overflow-hidden shadow p-4">
+          <div
+            className="text-center bg-[#8A4F0E] text-white text-lg font-semibold mb-3"
+            style={{
+              borderTopLeftRadius: "16px",
+              borderTopRightRadius: "16px",
+            }}
+          >
+            Template
+          </div>
+          {/* // 顯示 ON 狀態 */}
+          <LuckyExchangeButton isActive={true}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+          {/* // 顯示 OFF 狀態 */}
+          <LuckyExchangeButton isActive={false}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+        </div>
+        <div className="w-72 rounded-xl overflow-hidden shadow p-4">
+          <div
+            className="text-center bg-[#8A4F0E] text-white text-lg font-semibold mb-3"
+            style={{
+              borderTopLeftRadius: "16px",
+              borderTopRightRadius: "16px",
+            }}
+          >
+            Template
+          </div>
+          {/* // 顯示 ON 狀態 */}
+          <LuckyExchangeButton isActive={true}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+          {/* // 顯示 OFF 狀態 */}
+          <LuckyExchangeButton isActive={false}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+        </div>
+        <div className="w-72 rounded-xl overflow-hidden shadow p-4">
+          <div
+            className="text-center bg-[#8A4F0E] text-white text-lg font-semibold mb-3"
+            style={{
+              borderTopLeftRadius: "16px",
+              borderTopRightRadius: "16px",
+            }}
+          >
+            Template
+          </div>
+          {/* // 顯示 ON 狀態 */}
+          <LuckyExchangeButton isActive={true}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+          {/* // 顯示 OFF 狀態 */}
+          <LuckyExchangeButton isActive={false}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+        </div>
+        <div className="w-72 rounded-xl overflow-hidden shadow p-4">
+          <div
+            className="text-center bg-[#8A4F0E] text-white text-lg font-semibold mb-3"
+            style={{
+              borderTopLeftRadius: "16px",
+              borderTopRightRadius: "16px",
+            }}
+          >
+            Template
+          </div>
+          {/* // 顯示 ON 狀態 */}
+          <LuckyExchangeButton isActive={true}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+          {/* // 顯示 OFF 狀態 */}
+          <LuckyExchangeButton isActive={false}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+        </div>
+        <div className="w-72 rounded-xl overflow-hidden shadow p-4">
+          <div
+            className="text-center bg-[#8A4F0E] text-white text-lg font-semibold mb-3"
+            style={{
+              borderTopLeftRadius: "16px",
+              borderTopRightRadius: "16px",
+            }}
+          >
+            Template
+          </div>
+          {/* // 顯示 ON 狀態 */}
+          <LuckyExchangeButton isActive={true}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+          {/* // 顯示 OFF 狀態 */}
+          <LuckyExchangeButton isActive={false}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+        </div>
+        <div className="w-72 rounded-xl overflow-hidden shadow p-4">
+          <div
+            className="text-center bg-[#8A4F0E] text-white text-lg font-semibold mb-3"
+            style={{
+              borderTopLeftRadius: "16px",
+              borderTopRightRadius: "16px",
+            }}
+          >
+            Template
+          </div>
+          {/* // 顯示 ON 狀態 */}
+          <LuckyExchangeButton isActive={true}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+          {/* // 顯示 OFF 狀態 */}
+          <LuckyExchangeButton isActive={false}>
+            Lucky Exchange
+          </LuckyExchangeButton>
+        </div>
       </div>
     </div>
   );
