8f213c230c182fd686bff967bd1bf950886ff675
Vic428-human <z0983195379a@gmail.com>
2025-07-18 16:29:08 +0800

    feat: ContainerLayout 設計，分三個垂直區塊，左是菜單本體，中間是菜單點選後對應路由組件，右邊是展示DEMO

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useRef } from "react";
+import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import { useNavigate } from "react-router-dom";
+import SidebarMenu from "../Menu/SidebarMenu";
+import LuckySports from "../../components/LuckySports.tsx";
+import type { LuckySportsInstance } from "https://widget-dev-v3.ckex.xyz/mock/LuckySports.es.js";
+
+const { Sider } = Layout;
+
+const ContainerLayout = () => {
+  const lsDisplayRef = useRef<LuckySportsInstance | null>(null);
+  const navigate = useNavigate();
+
+  const isDisplay = (props) => {
+    const { title, value } = props;
+    const options = {
+      [title]: value,
+    };
+    console.log("====>options", options);
+    lsDisplayRef.current?.updateOptions({
+      options: options,
+    });
+  };
+  return (
+    <div className="flex h-screen">
+      <Sider
+        trigger={null}
+        collapsible
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+        width={150}
+        style={{
+          position: "relative",
+        }}
+      >
+        <div className="demo-logo-vertical"></div>
+        <div></div>
+        <SidebarMenu onMenuClick={({ key }) => navigate(key as string)} />
+      </Sider>
+      {/* demo   */}
+      <Outlet />
+      <div className="flex items-center justify-center w-[100%] p-2 text-center relative">
+        <div
+          className={`relative h-[90%] w-[1100px] overflow-auto transition-all duration-300 ease-in-out ${
+            true ? "w-full" : "w-[375px]"
+          }`}
+        >
+          <LuckySports ref={lsDisplayRef} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContainerLayout;
