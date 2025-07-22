94d6d9c7ed9880400e963b64071fdaae82b6d4f5
Vic428-human <z0983195379a@gmail.com>
2025-07-22 13:00:51 +0800

    clean provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 // https://ant.design/docs/spec/layout
 // Layout => https://ant.design/components/layout
 import MainContent from "./routes/MainContent.tsx";
-import { DisplayProvider } from "./context/DisplayContext";
 
 function App() {
-  return (
-    <DisplayProvider>
-      <MainContent />
-    </DisplayProvider>
-  );
+  return <MainContent />;
 }
 
 export default App;
