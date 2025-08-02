91297ec38ddd7295c0e9b30737b720b6b994dbf3
Vic428-human <z0983195379a@gmail.com>
2025-08-02 16:51:34 +0800

    feat: 加入回魔影片，跟刪除input邏輯

--- src/components/spComp.tsx
+++ src/components/spComp.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import videoSrc from "../videos/sp.mp4";
 type GearOption = {
   rate: number;
   price: number;
@@ -11,12 +11,11 @@ type ResultOption = GearOption & {
 };
 
 export default function ManaRecoveryCalculator() {
-  const [intValue, setIntValue] = useState(36);
-  const [spValue, setSpValue] = useState(127);
+  const [intValue, setIntValue] = useState(0);
+  const [spValue, setSpValue] = useState(69);
   const [options, setOptions] = useState<GearOption[]>([
-    { rate: 25, price: 180000 },
+    { rate: 0, price: 81000 },
   ]);
-  console.log("options", options);
   const [results, setResults] = useState<ResultOption[]>([]);
 
   const handleInputChange = (
@@ -32,6 +31,13 @@ export default function ManaRecoveryCalculator() {
   const addOption = () => {
     setOptions([...options, { rate: 0, price: 0 }]);
     calculate();
+  };
+
+  const removeOption = (idx: number) => {
+    if (idx >= 0 && idx < options.length) {
+      setOptions(options.filter((option, index) => index !== idx));
+      calculate();
+    }
   };
 
   const calculate = () => {
@@ -52,73 +58,95 @@ export default function ManaRecoveryCalculator() {
   };
 
   return (
-    <div className="w-1/2 mx-auto my-auto h-[800px] p-4 bg-white shadow-md rounded-lg">
+    <div className="w-1/2 mx-auto my-auto h-[760px] p-4 bg-white  rounded-lg">
       <div className="h-[300px] overflow-y-scroll overflow-x-hidden no-scrollbar">
-        <h2 className="text-2xl font-bold mb-4">RO回魔計算機</h2>
-        <div className="mb-4 space-x-2">
-          <label>
-            INT:
-            <input
-              type="number"
-              className="border rounded p-1 ml-2 w-24"
-              value={intValue}
-              onChange={(e) => setIntValue(+e.target.value)}
-            />
-          </label>
-          <label>
-            SP:
-            <input
-              type="number"
-              className="border rounded p-1 ml-2 w-24"
-              value={spValue}
-              onChange={(e) => setSpValue(+e.target.value)}
-            />
-          </label>
-        </div>
-
-        <div className="space-y-2 mb-6">
-          {options.map((option, idx) => (
-            <div key={idx} className="flex items-center gap-1">
+        <div className="flex">
+          {/* 左半邊 */}
+          <div>
+            <div className="mb-4 space-x-2">
               <label>
-                Rate (%):
+                INT:
                 <input
                   type="number"
                   className="border rounded p-1 ml-2 w-24"
-                  value={option.rate}
-                  onChange={(e) =>
-                    handleInputChange(idx, "rate", +e.target.value)
-                  }
+                  value={intValue}
+                  onChange={(e) => setIntValue(+e.target.value)}
                 />
               </label>
               <label>
-                裝備售價:
+                SP:
                 <input
                   type="number"
-                  className="border rounded p-1 ml-2 w-36"
-                  value={option.price}
-                  onChange={(e) =>
-                    handleInputChange(idx, "price", +e.target.value)
-                  }
+                  className="border rounded p-1 ml-2 w-24"
+                  value={spValue}
+                  onChange={(e) => setSpValue(+e.target.value)}
                 />
               </label>
-              {idx === options.length - 1 && (
-                <button
-                  onClick={addOption}
-                  className="ml-2 w-[30px] h-[30px] font-bold text-[20px] leading-[20px] cursor-pointer rounded-md flex items-center justify-center bg-transparent border-0 hover:bg-gray-200"
-                  aria-label="新增輸入框"
-                >
-                  +
-                </button>
-              )}
             </div>
-          ))}
+            <div className="space-y-2 mb-6">
+              {options.map((option, idx) => (
+                <div key={idx} className="flex items-center gap-1">
+                  <label>
+                    Rate (%):
+                    <input
+                      type="number"
+                      className="border rounded p-1 ml-2 w-24"
+                      value={option.rate}
+                      onChange={(e) =>
+                        handleInputChange(idx, "rate", +e.target.value)
+                      }
+                    />
+                  </label>
+                  <label>
+                    裝備售價:
+                    <input
+                      type="number"
+                      className="border rounded p-1 ml-2 w-36"
+                      value={option.price}
+                      onChange={(e) =>
+                        handleInputChange(idx, "price", +e.target.value)
+                      }
+                    />
+                  </label>
+                  {idx === options.length - 1 && (
+                    <div className="flex items-center">
+                      <button
+                        onClick={addOption}
+                        className="ml-2 w-[30px] h-[30px] font-bold text-[20px] leading-[20px] cursor-pointer rounded-md flex items-center justify-center bg-transparent border-0 hover:bg-gray-200"
+                        aria-label="新增輸入框"
+                      >
+                        +
+                      </button>
+                      <button
+                        onClick={() => removeOption(idx)}
+                        className="ml-2 w-[30px] h-[30px] font-bold text-[20px] leading-[20px] cursor-pointer rounded-md flex items-center justify-center bg-transparent border-0 hover:bg-gray-200"
+                        aria-label="刪除輸入框"
+                      >
+                        -
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+            <div className="">
+              <video
+                className="w-full max-w-2xl mx-auto rounded-lg object-fit"
+                controls
+              >
+                <source src={videoSrc} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+
+          {/* 右邊 */}
         </div>
       </div>
 
       <div
         className="overflow-y-scroll scrollbar-none overflow-x-hidden  no-scrollbar"
         style={{
-          maxHeight: "calc(100% - 500px)",
+          maxHeight: "calc(100% - 300px)",
         }}
       >
         {results.length > 0 && (
